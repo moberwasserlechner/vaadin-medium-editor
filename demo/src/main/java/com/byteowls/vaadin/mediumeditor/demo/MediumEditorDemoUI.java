560c4ffad2f84346a685b282bc93b93ae123c638
Michael Oberwasserlechner <michaelowl@gmail.com>
2016-01-26 23:26:53 +0100

    Demo 2 editors with different configs

--- demo/src/main/java/com/byteowls/vaadin/mediumeditor/demo/MediumEditorDemoUI.java
+++ demo/src/main/java/com/byteowls/vaadin/mediumeditor/demo/MediumEditorDemoUI.java
@@ -23,6 +23,10 @@ public class MediumEditorDemoUI extends UI {
 
   private Label preview = null;
 
+  private Button btnFormatted;
+
+  private Button btnPlain;
+
   @Override
   protected void init(VaadinRequest request) {
 
@@ -32,16 +36,18 @@ public class MediumEditorDemoUI extends UI {
     vl.addStyleName(ValoTheme.LAYOUT_WELL);
 
     preview = new Label();
-    MediumEditor editor = new MediumEditor();
-    editor.setSizeFull();
-    editor.setFocusOutlineEnabled(false);
-    editor.setJsLoggingEnabled(true);
-    editor.setContent(Lorem.getHtmlParagraphs(3, 3));
-    editor.addBlurListener(value -> {
+    preview.setContentMode(ContentMode.HTML);
+    
+    MediumEditor e1 = new MediumEditor();
+    e1.setSizeFull();
+    e1.setFocusOutlineEnabled(false);
+    e1.setJsLoggingEnabled(true);
+    e1.setContent(Lorem.getHtmlParagraphs(1, 3));
+    e1.addBlurListener(value -> {
       preview.setValue(value);
     });
-    editor.configure(
-        editor.options()
+    e1.configure(
+        e1.options()
         .fontawesomeButtonLabels()
         .toolbar()
           .buttons(BuildInButton.BOLD, BuildInButton.ITALIC, BuildInButton.H1, BuildInButton.JUSTIFY_CENTER)
@@ -55,27 +61,58 @@ public class MediumEditorDemoUI extends UI {
         .imageDragging(false)
         .done()
         );
+    
+    MediumEditor e2 = new MediumEditor();
+    e2.setSizeFull();
+    e2.setContent(Lorem.getHtmlParagraphs(1, 2));
+    e2.addBlurListener(value -> {
+      preview.setValue(value);
+    });
+    e2.configure(
+        e2.options()
+        .fontawesomeButtonLabels()
+        .toolbar()
+          .defaultButtons()
+          .add(BuildInButton.ORDEREDLIST, "Aufzaehlung")
+          .done()
+        .imageDragging(false)
+        .done()
+        );
 
+    HorizontalLayout editorHl = new HorizontalLayout(e1, e2);
+    editorHl.addStyleName(ValoTheme.LAYOUT_CARD);
+    editorHl.setSpacing(true);
+    editorHl.setSizeFull();
 
-    vl.addComponent(editor);
+    vl.addComponent(editorHl);
     vl.addComponent(preview);
 
-    Button btnFormatted = new Button("Preview formatted!");
+    btnFormatted = new Button("Preview formatted!");
+    btnFormatted.addStyleName(ValoTheme.BUTTON_FRIENDLY);
     btnFormatted.addClickListener(e -> {
+      btnFormatted.addStyleName(ValoTheme.BUTTON_FRIENDLY);
+      btnPlain.removeStyleName(ValoTheme.BUTTON_FRIENDLY);
       preview.setContentMode(ContentMode.HTML);
     });
 
-    Button btnPlain = new Button("Preview html source!");
+    btnPlain = new Button("Preview html source!");
     btnPlain.addClickListener(e -> {
+      btnPlain.addStyleName(ValoTheme.BUTTON_FRIENDLY);
+      btnFormatted.removeStyleName(ValoTheme.BUTTON_FRIENDLY);
       preview.setContentMode(ContentMode.TEXT);
     });
+    
+    Button readOnly = new Button("Toggle e2 ReadOnly");
+    readOnly.addClickListener(e -> {
+      e2.setReadOnly(!e2.isReadOnly());
+    });
 
-    HorizontalLayout btnHl = new HorizontalLayout(btnFormatted, btnPlain);
+    HorizontalLayout btnHl = new HorizontalLayout(btnFormatted, btnPlain, readOnly);
     btnHl.setSpacing(true);
     vl.addComponent(btnHl);
     vl.setComponentAlignment(btnHl, Alignment.BOTTOM_CENTER);
 
-    vl.setExpandRatio(editor, 1);
+    vl.setExpandRatio(editorHl, 1);
     vl.setExpandRatio(preview, 1);
     vl.setExpandRatio(btnHl, 0.2f);
 
